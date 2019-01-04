69e10f44ad5dc2fce28af621762d3496b431fb6c
voodoo971 <33722656+voodoo971@users.noreply.github.com>
2019-01-04 16:22:03 +0100

    Update autoluks.php

--- autoluks.php
+++ autoluks.php
@@ -1,80 +1,103 @@
 <?php
 include ('/var/www/sauvegarde/vendor/function.php');
+
+
 function mount_device_auto($repertoire,$luks,$uuid,$uuid_partition,$key){
-$luks_log= '/var/www/sauvegarde/log/autoluks.log';  
-$log = new Logging();  
+$luks_log= '/var/www/sauvegarde/log/autoluks.log';
+$log = new Logging();
 $log->lfile($luks_log);
-if(test_mount($repertoire)){
-$log->lwrite('LUKS >>>> Répertoire '.$repertoire.' déjà monté >>> OK');
-}
-else {
+
+
 if(isset($repertoire) || isset($luks) ||  isset($uuid) || isset($key)){
+
 if(open_luks($uuid,$luks,$key)){
 $log->lwrite('LUKS >>>> Partition LUKS '.$luks.' OK');
-}
-else {
- 
-$log->lwrite('LUKS >>>> Echec luksOpen '.$luks.'');     
-}
-if (mount_usb($uuid_partition,$repertoire)){
+
+mount_usb($uuid_partition,$repertoire);
 $log->lwrite('LUKS >>>> Réussite Montage '.$repertoire.' OK');
 }
-else
-{
-$log->lwrite('LUKS >>>> Echec Montage du Disque '.$luks.'');    
-open_luks($uuid,$luks,$key);
+else {
+$log->lwrite('LUKS >>>> Echec luksOpen '.$luks.'');
+$log->lwrite('LUKS >>>> Echec Montage du Disque '.$luks.'');
 }
 }
 }
-} 
 function check_usb($uuid,$luks,$repertoire)
 {
 $luks_log= '/var/www/sauvegarde/log/autoluks.log';
 $log = new Logging();
 $log->lfile($luks_log);
 $cmd = "blkid | grep $uuid";
-$start_cmd = exec($cmd,$output,$return_var_usb);
-if ( $return_var_usb == 0){
 
+
+$start_cmd = exec($cmd,$output,$return_var_usb);
+
+
+if ( $return_var_usb == 0){
 $log->lwrite('USB >>>> USB CONNECTE DISQUE '.$luks.' OK');
 $log->lwrite('USB >>>> CODE RETOUR '.$return_var_usb.'');
 }
 else
 {
-//close_device($repertoire,$luks); 
+close_device($repertoire,$luks);
 $log->lwrite('USB >>>> USB DECONNECTE DISQUE '.$luks.'');
 $log->lwrite('USB >>>> CLOSE DEVICE '.$repertoire.' >>>');
 $log->lwrite('USB >>>> CODE RETOUR '.$return_var_usb.'');
 }
-} 
+}
 if (PhpEstDejaLance($argv[0], 2) == true) {
     Trace_rouge("DEJA LANCE");
     exit();
 }
 while(1){
+
+  $luks_log= '/var/www/sauvegarde/log/autoluks.log';
+  $log = new Logging();
+  $log->lfile($luks_log);
+
+
 if(isset($uuid_1) || isset($luks_name_1) ||  isset($g_REPERTOIRE_DISQUE_1) ){
 check_usb($uuid_1,$luks_name_1,$g_REPERTOIRE_DISQUE_1);
+
 }
-/*
+
 if(isset($uuid_2) || isset($luks_name_2) ||  isset($g_REPERTOIRE_DISQUE_2) ){
 check_usb($uuid_2,$luks_name_2,$g_REPERTOIRE_DISQUE_2);
+
 }
 
-if(isset($uuid_3) || isset($luks_name_3) ||  isset($g_REPERTOIRE_DISQUE_3) ){
-check_usb($uuid_3,$luks_name_3,$g_REPERTOIRE_DISQUE_3);
+
+
+
+
+if(test_mount($g_REPERTOIRE_DISQUE_1)){
+  echo "LUKS >>>> Répertoire $g_REPERTOIRE_DISQUE_1 déjà monté >>> OK \n";
+$log->lwrite('LUKS >>>> Répertoire '.$g_REPERTOIRE_DISQUE_1.' déjà monté >>> OK');
+
 }
-*/
-if(isset($g_REPERTOIRE_DISQUE_1) || isset($luks_name_1) ||  isset($uuid_1) || isset($key_path_1)){
-mount_device_auto($g_REPERTOIRE_DISQUE_1,$luks_name_1,$uuid_1,$uuid_ext4,$key_path_1);
+else {
+Trace_rouge('LUKS >>>> Répertoire '.$g_REPERTOIRE_DISQUE_1.' non monté >>> Erreur');
+$log->lwrite('LUKS >>>> Répertoire '.$g_REPERTOIRE_DISQUE_1.' non monté >>> Erreur');
+  if(isset($g_REPERTOIRE_DISQUE_1) || isset($luks_name_1) ||  isset($uuid_1) || isset($key_path_1)){
+  mount_device_auto($g_REPERTOIRE_DISQUE_1,$luks_name_1,$uuid_1,$uuid_1_ext4,$key_path_1);
+  }
 }
-/*
+
+
+
+if(test_mount($g_REPERTOIRE_DISQUE_2)){
+    echo "LUKS >>>> Répertoire '.$g_REPERTOIRE_DISQUE_2.' déjà monté >>> OK \n";
+$log->lwrite('LUKS >>>> Répertoire '.$g_REPERTOIRE_DISQUE_2.' déjà monté >>> OK');
+}
+
+else{
+
+Trace_rouge('LUKS >>>> Répertoire '.$g_REPERTOIRE_DISQUE_2.' non monté >>> Erreur');
+$log->lwrite('LUKS >>>> Répertoire '.$g_REPERTOIRE_DISQUE_2.' non monté >>> Erreur');
 if(isset($g_REPERTOIRE_DISQUE_2) || isset($luks_name_2) ||  isset($uuid_2) || isset($key_path_2)){
-mount_device_auto($g_REPERTOIRE_DISQUE_2,$luks_name_2,$uuid_2,$key_path_2);
+mount_device_auto($g_REPERTOIRE_DISQUE_2,$luks_name_2,$uuid_2,$uuid_2_ext4,$key_path_2);
+}
 }
 
-if(isset($g_REPERTOIRE_DISQUE_3) || isset($luks_name_3) ||  isset($uuid_3) || isset($key_path_3)){
-mount_device_auto($g_REPERTOIRE_DISQUE_3,$luks_name_3,$uuid_3,$key_path_3);
-}
-*/
 sleep(15);
 }
